44889b7c46a4e7e071be886231175126ca469acd
luanfreitasdev <luanfreitasgoncalves@gmail.com>
2021-05-01 14:15:41 -0300

    powergrid - live beer and code

---
+++ app/Http/Livewire/DishesTable.php
@@ -0,0 +1,225 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Carbon\Carbon;
+use App\Models\Dish;
+use App\Models\Kitchen;
+use App\Models\Category;
+use Illuminate\Support\Str;
+use Illuminate\Support\Collection;
+use Illuminate\Database\QueryException;
+use PowerComponents\LivewirePowerGrid\Button;
+use PowerComponents\LivewirePowerGrid\Column;
+use PowerComponents\LivewirePowerGrid\PowerGrid;
+use PowerComponents\LivewirePowerGrid\PowerGridComponent;
+use PowerComponents\LivewirePowerGrid\Traits\ActionButton;
+
+class DishesTable extends PowerGridComponent
+{
+    use ActionButton;
+
+    public function setUp()
+    {
+        $this->showCheckBox()
+            ->showPerPage()
+            ->showRecordCount()
+            ->showSearchInput();
+    }
+
+    public function dataSource(): array
+    {
+        $model = Dish::query()->with('kitchen', 'category')->get();
+        return PowerGrid::eloquent($model)
+            ->addColumn('id')
+            ->addColumn('name')
+            ->addColumn('calories')
+            ->addColumn('calories', function (Dish $dish) {
+                return  $dish->calories . ' kcal';
+            })
+            /*** CATEGORY ***/
+            ->addColumn('category_id', function (Dish $dish) {
+                return  $dish->category_id;
+            })
+            ->addColumn('category_name', function (Dish $dish) {
+                return  $dish->category->name;
+            })
+
+            /*** KITCHEN ***/
+            ->addColumn('kitchen_id', function (Dish $dish) {
+                return  $dish->kitchen_id;
+            })
+            ->addColumn('kitchen_name', function (Dish $dish) {
+                return  $dish->kitchen->name;
+            })
+
+            /*** PRICE ***/
+            ->addColumn('price')
+            ->addColumn('price_BRL', function (Dish $dish) {
+                return  'R$ ' . number_format($dish->price, 2,',', '.'); //R$ 1.000,00
+            })
+
+            /*** SALE'S PRICE ***/
+            ->addColumn('sales_price')
+            ->addColumn('sales_price_BRL', function (Dish $dish) {
+                $sales_price = $dish->price + ($dish->price * 0.15);
+                return  'R$ ' . number_format($sales_price, 2, ',', '.'); //R$ 1.000,00
+            })
+
+            /*** STOCK ***/
+            ->addColumn('in_stock')
+            ->addColumn('in_stock_label', function (Dish $dish) {
+                return ($dish->in_stock ? "sim" : "não");
+            })
+
+            /*** Produced At ***/
+            ->addColumn('produced_at')
+            ->addColumn('produced_at_formatted', function(Dish $dish) {
+                return Carbon::parse($dish->produced_at)->format('d/m/Y');
+            })
+            ->make();
+    }
+
+    public function columns(): array
+    {
+
+        $canEdit = true; //Permissão pra editar
+
+        return [
+
+            Column::add()
+                ->title(__('ID'))
+                ->field('id')
+                ->searchable()
+                ->sortable(),
+
+            Column::add()
+                ->title(__('Prato'))
+                ->field('name')
+                ->searchable()
+                ->editOnClick($canEdit)
+                ->clickToCopy(true)
+                ->makeInputText('name')
+                ->sortable(),
+
+            Column::add()
+                ->title(__('Categoria'))
+                ->field('category_name')
+                ->makeInputMultiSelect(Category::all(), 'name', 'category_id'),
+
+            Column::add()
+                ->title(__('Preço'))
+                ->field('price_BRL')
+                ->editOnClick($canEdit)
+                ->makeInputRange('price', ".", ","),
+
+            Column::add()
+                ->title(__('Preço de Venda'))
+                ->field('sales_price_BRL'),
+
+            Column::add()
+                ->title(__('Calorias'))
+                ->field('calories')
+                ->makeInputRange('calories')
+                ->sortable(),
+
+            Column::add()
+                ->title(__('Em Estoque'))
+                ->toggleable(false, 'sim', 'não')
+                ->makeBooleanFilter('in_stock', 'sim', 'não')
+                ->field('in_stock'),
+
+            Column::add()
+                ->title(__('Cozinha'))
+                ->field('kitchen_name')
+                ->sortable()
+                ->makeInputMultiSelect(Kitchen::all(), 'name', 'kitchen_id'),
+            /*
+                        Column::add()
+                            ->title(__('cooked'))
+                            ->field('produced_at')
+                            ->hidden(),
+            */
+            Column::add()
+                ->title(__('Data de produção'))
+                ->field('produced_at_formatted')
+                ->makeInputDatePicker('produced_at')
+        ];
+
+    }
+
+    /*
+    |--------------------------------------------------------------------------
+    | Actions Method
+    |--------------------------------------------------------------------------
+    | Enable this section only when you have defined routes for these actions.
+    |
+    */
+
+
+    public function actions(): array
+    {
+        return [
+            Button::add('edit')
+                ->caption(__('Editar'))
+                ->class('bg-indigo-500 text-white')
+                ->route('dish.edit', ['dish' => 'id']),
+
+            Button::add('destroy')
+                ->caption(__('Deletar'))
+                ->class('bg-red-500 text-white')
+                ->route('dish.destroy', ['dish' => 'id'])
+                ->method('delete')
+        ];
+    }
+
+
+    /*
+    |--------------------------------------------------------------------------
+    | Edit Method
+    |--------------------------------------------------------------------------
+    | Enable this section to use editOnClick() or toggleable() methods
+    |
+    */
+
+
+    public function update(array $data ): bool
+    {
+
+        //Clean price_BRL R$ 4.947,70 --> 44947.70 and saves in database field 'price'
+        if ($data['field'] == 'price_BRL') {
+            $data['field'] = 'price';
+            $data['value'] = Str::of($data['value'])
+                ->replace('.', '')
+                ->replace(',', '.')
+                ->replaceMatches('/[^Z0-9\.]/', '');
+        }
+
+        try {
+            $updated = Dish::query()->find($data['id'])->update([
+                $data['field'] => $data['value']
+            ]);
+        } catch (QueryException $exception) {
+            $updated = false;
+        }
+        return $updated;
+    }
+
+    public function updateMessages(string $status, string $field = '_default_message'): string
+    {
+        $updateMessages = [
+            'success'   => [
+                '_default_message' => __('Data has been updated successfully!'),
+                'price_BRL' => __('Preço alterado'),
+                //'custom_field' => __('Custom Field updated successfully!'),
+            ],
+            "error" => [
+                '_default_message' => __('Error updating the data.'),
+                //'custom_field' => __('Error updating custom field.'),
+            ]
+        ];
+
+        return ($updateMessages[$status][$field] ?? $updateMessages[$status]['_default_message']);
+    }
+
+}
---
+++ config/livewire-powergrid.php
@@ -0,0 +1,91 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Template
+    |--------------------------------------------------------------------------
+    |
+    | You will be able to know which theme pattern will be loaded
+    | bootstrap or tailwind
+    |
+    */
+    'theme' => 'tailwind',
+
+    /*
+    |--------------------------------------------------------------------------
+    | Theme Version
+    |--------------------------------------------------------------------------
+    |
+    | Here you define which version and bootstrap or tailwind you are using
+    | >= bootstrap@5.0.0-beta3 working with bootstrap-select
+    |
+    */
+
+    'theme_versions' => [
+        'bootstrap' => '50',
+        /*
+         * https://tailwindcss.com
+         */
+        'tailwind' => '2'
+    ],
+
+    /*
+    |--------------------------------------------------------------------------
+    | Plugins
+    |--------------------------------------------------------------------------
+    |
+    | Plugins used: bootstrap-select when bootstrap, flatpicker.js to datepicker
+    |
+    */
+    'plugins' => [
+        /*
+         * https://github.com/snapappointments/bootstrap-select
+         */
+        'bootstrap-select' => [
+            'js' => 'https://cdnjs.cloudflare.com/ajax/libs/bootstrap-select/1.14.0-beta2/js/bootstrap-select.min.js',
+            'css' => 'https://cdnjs.cloudflare.com/ajax/libs/bootstrap-select/1.14.0-beta2/css/bootstrap-select.min.css'
+        ],
+        /*
+         * https://flatpickr.js.org
+         */
+        'flat_piker' => [
+            'js' => 'https://cdn.jsdelivr.net/npm/flatpickr',
+            'css' => 'https://cdn.jsdelivr.net/npm/flatpickr/dist/flatpickr.min.css',
+            'translate' => (app()->getLocale() != 'en') ? 'https://npmcdn.com/flatpickr/dist/l10n/'.\Illuminate\Support\Str::substr(app()->getLocale(), 0, 2).'.js': '',
+            'locales' => [
+                'pt_BR' => [
+                    'locale' => 'pt',
+                    'dateFormat' => 'd/m/Y H:i',
+                    'enableTime' => true,
+                    'time_24hr' => true
+                ]
+            ]
+        ]
+    ],
+
+    /*
+    |--------------------------------------------------------------------------
+    | Filters
+    |--------------------------------------------------------------------------
+    |
+    | You choose which type of filter you want to use inline
+    | to filter inside the table or outside the table
+    | 'inline' or 'outside'
+    |
+    */
+
+    'filter' => 'inline',
+
+    /*
+    |--------------------------------------------------------------------------
+    | Cache
+    |--------------------------------------------------------------------------
+    |
+    | When the data is cached, the search is much faster.
+    | It is updated whenever the page is reloaded or a field is changed
+    |
+    */
+    'cached_data' => true
+];
