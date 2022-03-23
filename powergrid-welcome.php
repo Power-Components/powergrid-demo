d6b1da3bfd9c01a269974d59cd1493941091aa95
Daniel Ang <daniel@sysanalyst.eu>
2022-03-24 00:51:24 +0100

    welcome script
    - This script takes care of removing PHP 8.1 features if needed

---
+++ powergrid-welcome.php
@@ -0,0 +1,49 @@
+<?php
+
+declare(strict_types = 1);
+
+$green    = "\033[0;32m";
+$yellow   = "\033[0;33m";
+$noColor  = "\033[0m";
+
+echo <<<EOF
+
+{$yellow} __{$green}     ____                          ______     _     __
+{$yellow}/ /_,{$green}  / __ \____ _      _____  _____/ ____/____(_)___/ /
+{$yellow}/_ ,'{$green} / /_/ / __ \ | /| / / _ \/ ___/ / __/ ___/ / __  / 
+{$yellow}/'{$green}   / ____/ /_/ / |/ |/ /  __/ /  / /_/ / /  / / /_/ /  
+    /_/    \____/|__/|__/\___/_/   \____/_/  /_/\__,_/     
+
+
+{$noColor}Welcome and thank you for downloading our Demo!
+
+
+📚 See the Documentation at {$yellow}https://livewire-powergrid.com/{$noColor} for more information.
+
+
+⭐ Please consider {$yellow}starring{$noColor} our repository at {$yellow}https://github.com/Power-Components/livewire-powergrid{$noColor} ⭐
+
+
+EOF;
+
+/*
+|*********************************************************************
+|                     PHP Version Compatibility
+|*********************************************************************
+| Although PowerGrid Demo repository requires PHP 8.0 as minimum version,
+| we have decided to include a commented version of the 'Enum Filter' feature.
+| Enum is available from Php 8.1+ and the script below removes comments 
+| in certain files, guaranteeing that nothing will break for your PHP version.
+*/
+
+if (version_compare(PHP_VERSION, '8.1', '>')) {
+    $files = ['app/Http/Enums/Diet.php', 'app/Http/Livewire/DishesTable.php'];
+
+    foreach ($files as $filePath) {
+        $fileContent = file_get_contents($filePath);
+    
+        $fileContent = preg_replace('/^.*Only from Php 8.1[^\\n]*/m', "", $fileContent);
+
+        file_put_contents($filePath, $fileContent);
+    }
+}
