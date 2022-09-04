c7f7bc50de72329a8d460e63135a017777920067
Dan Ang <79267265+dansysanalyst@users.noreply.github.com>
2022-09-05 01:18:37 +0200

    Improving detail view

--- resources/views/components/detail.blade.php
+++ resources/views/components/detail.blade.php
@@ -2,4 +2,8 @@
     <p>
         Dish has {{ $row->calories }} ({{ $options['message'] }})
     </p>
+
+    <div class="flex justify-begin">
+        <button wire:click.prevent="toggleDetail('{{ $id }}')" class="mt-2 p-1 text-xs bg-red-600 text-white rounded-lg">Close</button>
+    </div>
 </div>
