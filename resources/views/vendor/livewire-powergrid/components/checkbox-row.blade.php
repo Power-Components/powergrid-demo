8a33851b3d5d51b262c321a66d446db0284184e7
Dan Ang <daniel@sysanalyst.eu>
2023-04-24 23:31:22 +0200

    views

---
+++ resources/views/vendor/livewire-powergrid/components/checkbox-row.blade.php
@@ -0,0 +1,38 @@
+@if($checkbox)
+    @if($ruleHide)
+        <td class="{{ $theme->checkbox->thClass }}"
+            style="{{ $theme->checkbox->thStyle }}">
+        </td>
+    @elseif($ruleDisable)
+        <td class="{{ $theme->checkbox->thClass }}" style="{{ $theme->thStyle }}">
+            <div class="{{ $theme->checkbox->divClass }}">
+                <label class="{{ $theme->checkbox->labelClass }}">
+                    <input @if(isset($ruleSetAttribute['attribute']))
+                           {{ $attributes->merge([$ruleSetAttribute['attribute'] => $ruleSetAttribute['value']])->class($theme->checkbox->inputClass) }}
+                           @else
+                           class="{{ $theme->checkbox->inputClass }}"
+                           @endif
+                           disabled
+                           type="checkbox">
+                </label>
+            </div>
+        </td>
+    @else
+        <td class="{{ $theme->checkbox->thClass }}"
+            style="{{ $theme->checkbox->thStyle }}">
+            <div class="{{ $theme->checkbox->divClass }}">
+                <label class="{{ $theme->checkbox->labelClass }}">
+                    <input @if(isset($ruleSetAttribute['attribute']))
+                           {{ $attributes->merge([$ruleSetAttribute['attribute'] => $ruleSetAttribute['value']])->class($theme->checkbox->inputClass) }}
+                           @else
+                           class="{{ $theme->checkbox->inputClass }}"
+                           @endif
+                           type="checkbox"
+                           x-on:click="window.Alpine.store('pgBulkActions').add($event.target.value, '{{ $tableName }}')"
+                           wire:model.defer="checkboxValues"
+                           value="{{ $attribute }}">
+                </label>
+            </div>
+        </td>
+    @endif
+@endif
