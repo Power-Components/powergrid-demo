496839d73cad4118a71379a5a0045646ec6d107c
Daniel <daniel@sysanalyst.eu>
2024-01-18 21:55:57 +0100

    merge smoke test

--- tests/Feature/SmokeTest.php
+++ tests/Feature/SmokeTest.php
@@ -7,7 +7,5 @@ use function Pest\Laravel\{get};
 test('All components are accessible', function () {
 
     ListComponents::handle()->map(fn ($component) => str($component)->before('Table')->kebab()->toString())
-        ->each(function ($route) {
-            get(route('default', ['table' => $route]))->assertOk();
-        });
+        ->each(fn ($route) => get(route('default', ['table' => $route]))->assertOk());
 });
