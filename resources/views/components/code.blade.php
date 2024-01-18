24c5375b30fc62a28ba6e0a1ae17351b6bdd86f5
Daniel <daniel@sysanalyst.eu>
2024-01-18 21:58:05 +0100

    WIP: source code and about

--- resources/views/components/code.blade.php
+++ resources/views/components/code.blade.php
@@ -24,8 +24,7 @@
                     <span class="text-gray-400">Code:</span>
                     <button class="code bg-gray-800 hover:bg-gray-700 text-gray-300 px-3 py-1 rounded-md" data-clipboard-target="#code" > Copy </button> </div>
                 <div class="overflow-x-auto">
-                    <pre id="code" class="text-gray-300" >
-
+                    <pre id="code" class="text-gray-300">
                         @if (empty(env('TORCHLIGHT_TOKEN')))
                             <code>{{ $slot }}</code>
                         @else
--- resources/views/table.blade.php
+++ resources/views/table.blade.php
@@ -1,7 +1,17 @@
+@use('App\Models\Flight')
+
 @extends('layouts.base')
 
 @section('title')
     {{ $title }}
+@endsection
+
+@section('source_code')
+    <x-code>{{ $source_code }}</x-code>
+@endsection
+
+@section('about')
+    {!! $about !!}
 @endsection
 
 @section('main')
