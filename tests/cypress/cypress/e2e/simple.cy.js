ad6775aa83957d234c8e33d5ee5c4d7c093761ec
Daniel <daniel@sysanalyst.eu>
2024-01-19 22:19:13 +0100

    Simply Cypress

--- tests/cypress/cypress/e2e/simple.cy.js
+++ tests/cypress/cypress/e2e/simple.cy.js
@@ -2,33 +2,27 @@ import moment from "moment";
 
 describe('simple', () => {
     it('can visit simple page', () => {
-        cy.visit('/simple')
+        cy.visit('/examples/simple')
         cy.contains('Simple')
     })
 
     it('should be able to search properly', () => {
-        cy.visit('/simple')
+        cy.visit('/examples/simple')
+        cy.contains('Source Code')
         cy.contains('Simple')
+        cy.contains('Pastel de Nata')
 
-        cy.get('[data-cy=pg-search-default]').
-            type('Bife')
+        cy.get('input').should('have.attr', 'placeholder', 'Search...').type('Bife')
 
         cy.wait(1000) // wait for livewire debounce 700
 
-        const createdAt = moment().format('DD/MM/YYYY')
-
-        const data = [
-            '4', 'Bife à Rolê', 'Vitor', '$100.13', 'No', ''+createdAt,
-            '11', 'Bife à Parmegiana', 'Luan', '$110.20', 'Yes', ''+createdAt,
-            '45', 'Bife à Milanesa', 'Dan', '$100.10', 'No', ''+createdAt,
-        ];
-
-        cy.get('table tbody tr td').then(($td) => {
-            const texts = Cypress._.map($td, 'innerText')
-
-            expect(texts, 'data').to.deep.equal(data)
-        })
+        cy.contains('Bife à Rolê')
+        cy.contains('Bife à Parmegiana')
+        cy.contains('Bife à Milanesa')
+        cy.contains('Pastel de Nata').should('not.exist');
 
         cy.contains('Showing 1 to 3 of 3 Results')
+        
+        cy.get('a:contains("Source Code")').click()
     })
 })
