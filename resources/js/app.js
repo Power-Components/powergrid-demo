bc53eb151513fca3a36d6f6ae0ee262c7573ae30
Daniel <daniel@sysanalyst.eu>
2024-01-19 18:31:35 +0100

    Add clipboardJS

--- resources/js/app.js
+++ resources/js/app.js
@@ -5,6 +5,19 @@ import flatpickr from "flatpickr";
 import TomSelect from "tom-select";
 window.TomSelect = TomSelect
 
+import ClipboardJS from "clipboard";
+
+
+var clipboard = new ClipboardJS('.code')
+
+clipboard.on('success', function(e) {
+    let oldtext = e.trigger.textContent
+    e.trigger.textContent = '✅ Copied!'
+    e.clearSelection();
+    setTimeout(() => e.trigger.textContent = oldtext, 2000)
+});
+
+
 import './../../vendor/power-components/livewire-powergrid/dist/powergrid'
 
 Livewire.start();
