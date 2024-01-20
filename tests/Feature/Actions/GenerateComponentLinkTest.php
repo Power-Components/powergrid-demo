ce077a3d5baf12b5fe32190c58f94e3edb2a0c08
Dan <daniel@sysanalyst.eu>
2024-01-20 18:29:03 +0100

    fix test with old url

--- tests/Feature/Actions/GenerateComponentLinkTest.php
+++ tests/Feature/Actions/GenerateComponentLinkTest.php
@@ -5,4 +5,4 @@ use App\Actions\GenerateComponentLink;
 it('generates the component link')
     ->with('valid_component_candidates')
     ->expect(fn (string $candidate) => GenerateComponentLink::handle($candidate))
-    ->toBe('https://github.com/Power-Components/powergrid-demo-misc/blob/powergrid_v5/app/Livewire/FooBarTable.php');
+    ->toBe('https://github.com/Power-Components/powergrid-demo/blob/powergrid_v5/app/Livewire/FooBarTable.php');
