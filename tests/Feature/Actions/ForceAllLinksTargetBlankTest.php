57dbb3e5f1cf594789a91e1f7c9c31c0c1a9e2cf
Dan <daniel@sysanalyst.eu>
2024-01-20 23:41:57 +0100

    fix test

--- tests/Feature/Actions/ForceAllLinksTargetBlankTest.php
+++ tests/Feature/Actions/ForceAllLinksTargetBlankTest.php
@@ -2,22 +2,19 @@
 
 use App\Actions\ForceAllLinksTargetBlank;
 
-it('properly change external links to target="_blank"')
-    ->expect(fn () => ForceAllLinksTargetBlank::handle(html()))
-    ->toBe(cleanHtml());
+it('properly change external links to target="_blank"', function () {
+    expect(ForceAllLinksTargetBlank::handle($this->html))->toBe($this->cleanHtml);
+});
 
-function html()
-{
-    return <<<'HTML'
+beforeEach(function () {
+    $this->html = <<<'HTML'
 <p>aVisit <a href="https://livewire-powergrid.com">PowerGrid</a> documentation and read more about <a href="/examples/detail">Detail</a>.</p>
 <p>In this page you see <a href="#anchor">Anchor</a>.</p>
 HTML;
-}
 
-function cleanHtml()
-{
-    return <<<'HTML'
+    $this->cleanHtml = <<<'HTML'
 <p>aVisit <a href="https://livewire-powergrid.com" target="_blank">PowerGrid</a> documentation and read more about <a href="/examples/detail">Detail</a>.</p>
 <p>In this page you see <a href="#anchor">Anchor</a>.</p>
 HTML;
-}
+
+});
