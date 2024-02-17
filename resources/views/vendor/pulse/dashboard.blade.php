f061d928d935c5149eb77fdab790612aa7b2a0b7
luanfreitasdev <luanfreitas10@protonmail.com>
2024-02-17 18:47:45 -0300

    Add Pulse

---
+++ resources/views/vendor/pulse/dashboard.blade.php
@@ -0,0 +1,13 @@
+<x-pulse>
+    <livewire:pulse.servers cols="full" />
+
+    <livewire:pulse.cache cols="4" />
+
+    <livewire:pulse.slow-queries cols="8" />
+
+    <livewire:pulse.exceptions cols="6" />
+
+    <livewire:pulse.slow-requests cols="6" />
+
+    <livewire:powergrid-measurement-card cols="6" />
+</x-pulse>
