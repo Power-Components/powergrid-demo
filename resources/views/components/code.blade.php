5b92e13781ae239e716f840f3427dd60cf73a8c7
Dan <79267265+dansysanalyst@users.noreply.github.com>
2024-05-18 14:37:49 +0200

    FIX

--- resources/views/components/code.blade.php
+++ resources/views/components/code.blade.php
@@ -1,3 +1,6 @@
+@php
+    $hasTorchLight = !empty(env('TORCHLIGHT_TOKEN'));
+@endphp
 <div class="relative mx-auto text-sm">
     <div class="bg-pg-primary-900 text-white p-4 rounded-md">
         <div class="flex justify-between items-center mb-4">
@@ -16,12 +19,17 @@
                 id="code"
                 class="text-gray-300 text-sm"
             >
-@if (empty(env('TORCHLIGHT_TOKEN')))
-<code>{{ $example->sourceCode() }}</code>
-@else
+@if ($hasTorchLight)
 <x-torchlight-code language="php" theme="material-theme-darker">{!! $example->sourceCode() !!}</x-torchlight-code>
+@else
+<code>{{ $example->sourceCode() }}</code>
 @endif
             </pre>
         </div>
     </div>
+@if ($hasTorchLight)
+<div class="mt-2">
+    <small>Code highlighting provided by <a href="https://torchlight.dev/" target="_blank">Torchlight.dev</a></small>
+</div>
+@endif
 </div>
