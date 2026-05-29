833ac80c306baaf81787c7bffc157eb21c367f9d
Dan <daniel@sysanalyst.eu>
2026-05-29 13:07:52 +0200

    fix

--- resources/views/livewire/edit-stock.blade.php
+++ resources/views/livewire/edit-stock.blade.php
@@ -4,12 +4,6 @@
     <div class="py-2">
         DishId: {{ $dishId }}
 
-        <x-toggle wire:model="inStock" label="In Stock" />
-
-        <div class="space-x-2 flex justify-end mt-3">
-            <x-button flat label="Cancel" wire:click="cancel"/>
-            <x-button primary label="Save" wire:click="confirm"/>
-        </div>
     </div>
 
 </div>
